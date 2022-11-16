4ecb404c50cdd6aa77cad993b830faf8e121f481
Wesley-waka <wesley.waka@student.moringaschool.com>
2022-11-17 02:05:58 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,40 @@
+var a=10;
+var b=5;
+
+function add(a,b){
+    return add(1+3);
+}
+function subtract(a,b){
+    return subtract(1-3);
+}
+function multiply(a,b){
+    return multiply(1*3);
+}
+function divide(a,b){
+    return divide(10/5);
+}
+function add(a, b){
+    return a+b;
+}
+function subtract(a, b){
+    return a-b;
+}
+function multiply(a, b){
+    return a*b;
+}
+function divide(a, b){
+    return a/b;
+}
+function increment(a){
+    return (++a);
+}
+function decrement(a) {
+    return (--a);
+}
+function makeInt(n){
+    return parseInt (n,10);
+}
+function preserveDecimal(n){
+    return parseFloat(n);
+}
+
